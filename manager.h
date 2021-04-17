52b423441723c3ab9e5fbf5fbcd4e4bbd674db05
Jueun20 <22000350@handong.edu>
2021-04-17 15:32:55 +0900

    add manager.h_comment about selectDataNum()

--- manager.h
+++ manager.h
@@ -3,7 +3,7 @@
 int selectMenu();
 int loadData(Product *p); //데이터 불러오기
 void listProduct(Product *p, int count); //1. 제품 list 조회하기_R
-int selectDataNum(Product *p, int count); 
+int selectDataNum(Product *p, int count); //번호 고르기
 void saveData(Product *p, int count); //5. 제품 파일에 저장하기
 void searchName(Product *p, int count); //6. 제품 검색하기_이름
 void searchWeight(Product *p, int count); //7. 제품 검색하기_중량
