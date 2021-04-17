7735989915f6240bbab6c62003e5b18d81f800ae
Jueun20 <22000350@handong.edu>
2021-04-17 15:25:21 +0900

    change comment about listProduct()

--- manager.h
+++ manager.h
@@ -2,8 +2,8 @@
 
 int selectMenu();
 int loadData(Product *p); //데이터 불러오기
-void listProduct(Product *p, int count); //1. 제품 조회하기_R
-int selectDataNum(Product *p, int count);
+void listProduct(Product *p, int count); //1. 제품 list 조회하기_R
+int selectDataNum(Product *p, int count); 
 void saveData(Product *p, int count); //5. 제품 파일에 저장하기
 void searchName(Product *p, int count); //6. 제품 검색하기_이름
 void searchWeight(Product *p, int count); //7. 제품 검색하기_중량
